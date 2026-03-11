8daec62de094e4bfd2480b57067c256fbe6ccad2
Jair <droidjair95@gmail.com>
2026-03-11 11:40:27 -0600

    Agregar archivos de proyecto.

---
+++ QUIZTEAM/DB.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZTEAM
+{
+    internal class DB
+    {
+        private static string connStr = "Server=gondola.proxy.rlwy.net;Port=42614;Database=railway;Uid=root;Pwd=MNzmTeajpSCLHElCQguamXzDqEkdhZqN;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connStr);
+        }
+    }
+}
