a2650629d50271576b0ebdf0d1b398252ba10753
karenmich <kzarazuahdz@gmail.com>
2026-03-22 21:42:38 -0600

    Creación de Form juego con clase pregunta para mostrar preguntas de texto o imagen

---
+++ QUIZTEAM/Juego.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace QUIZTEAM
+{
+    public class Pregunta
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public string Tipo { get; set; } // "texto" o "imagen"
+        public List<string> Opciones { get; set; } = new List<string>();
+        public List<string> ImagenesOpciones { get; set; } = new List<string>();
+        public int IndiceCorrecta { get; set; }
+    }
+}
