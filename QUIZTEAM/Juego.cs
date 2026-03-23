cf0dfb37a44d0f3c690d4c52d5b781e09b357b08
karenmich <kzarazuahdz@gmail.com>
2026-03-22 21:45:18 -0600

    Llama a funciones, carga preguntas  y opciones con o sin imagen

--- QUIZTEAM/Juego.cs
+++ QUIZTEAM/Juego.cs
@@ -43,4 +43,65 @@ namespace QUIZTEAM
             this.BackColor = Color.FromArgb(26, 26, 46);
             this.Load += Juego_Load;
         }
+
+        private void Juego_Load(object sender, EventArgs e)
+        {
+            CargarPreguntas();
+            CalcularZonas();
+            CargarImagenesActual();
+            this.Invalidate();
+        }
+
+        private void CargarPreguntas()
+        {
+            _preguntas.Clear();
+            try
+            {
+                using (var conn = DB.GetConnection())
+                {
+                    conn.Open();
+                    // Obtener id de categoría
+                    int catId = 0;
+                    var cmdCat = new MySqlCommand(
+                        "SELECT id FROM categorias WHERE nombre=@n LIMIT 1", conn);
+                    cmdCat.Parameters.AddWithValue("@n", _categoria);
+                    var r = cmdCat.ExecuteReader();
+                    if (r.Read()) catId = r.GetInt32(0);
+                    r.Close();
+
+                    // Obtener preguntas aleatorias (10)
+                    var cmd = new MySqlCommand(
+                        @"SELECT id, texto, tipo, opcion1, opcion2, opcion3, opcion4,
+                          img1, img2, img3, img4, correcta
+                          FROM preguntas WHERE categoria_id=@c
+                          ORDER BY RAND() LIMIT 10", conn);
+                    cmd.Parameters.AddWithValue("@c", catId);
+                    var rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        var p = new Pregunta
+                        {
+                            Id = rd.GetInt32("id"),
+                            Texto = rd.GetString("texto"),
+                            Tipo = rd.GetString("tipo"),
+                            IndiceCorrecta = rd.GetInt32("correcta") - 1
+                        };
+                        p.Opciones.Add(rd.GetString("opcion1"));
+                        p.Opciones.Add(rd.GetString("opcion2"));
+                        p.Opciones.Add(rd.GetString("opcion3"));
+                        p.Opciones.Add(rd.GetString("opcion4"));
+                        p.ImagenesOpciones.Add(rd.IsDBNull(rd.GetOrdinal("img1")) ? "" : rd.GetString("img1"));
+                        p.ImagenesOpciones.Add(rd.IsDBNull(rd.GetOrdinal("img2")) ? "" : rd.GetString("img2"));
+                        p.ImagenesOpciones.Add(rd.IsDBNull(rd.GetOrdinal("img3")) ? "" : rd.GetString("img3"));
+                        p.ImagenesOpciones.Add(rd.IsDBNull(rd.GetOrdinal("img4")) ? "" : rd.GetString("img4"));
+                        _preguntas.Add(p);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error cargando preguntas: " + ex.Message);
+            }
+        }
+
     }
