b34745dbe63777a99b187a73fe98af05eeb2491d
Jair <Jair@gmail.com>
2026-04-28 12:54:02 -0600

    Se agrego clase Models

---
+++ QUIZTEAM/Models.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace QUIZTEAM
+{
+    public class Pregunta
+    {
+        public int id { get; set; }
+        public string texto { get; set; }
+        public string tipo { get; set; } // "texto" o "imagen"
+        public List<string> opciones { get; set; }
+        public List<string> imagenesOpciones { get; set; }
+        public int correcta { get; set; } // Valor de 1 a 4
+    }
+
+    public class PlayerScore
+    {
+        public string nombre { get; set; }
+        public int puntos { get; set; }
+        public int correctas { get; set; }
+    }
+}
